b0565d5acac91a47eff70638c8206a53889e8243
Tanuj Gyan <tanuj.gyan@teranet.ca>
2021-03-09 16:18:45 -0500

    Level order traversal

--- BinarySearchTreeImplementation/BinaryTree.cs
+++ BinarySearchTreeImplementation/BinaryTree.cs
@@ -8,9 +8,9 @@ namespace BinarySearchTreeImplementation
     {
         public Node Root { get; set; }
         public Node TempRoot { get; set; }
+        Queue<Node> queue = new Queue<Node>();
         public void Add(int value)
         {
-           
             if (this.Root == null)
             {
                 this.Root = new Node();
@@ -54,7 +54,6 @@ namespace BinarySearchTreeImplementation
 
             }
         }
-        
         public void TraversePreOrderIteration(Node rootNode)
         {
             
@@ -87,6 +86,90 @@ namespace BinarySearchTreeImplementation
                 TraversePreOrderRecursion(rootNode.RightNode);
             }
            
+        }
+        public void TraverseInOrderRecursion(Node rootNode)
+        {
+
+            if (rootNode != null)
+            {
+                TraverseInOrderRecursion(rootNode.LeftNode);
+                Console.Write(rootNode.Data + " ");
+                TraverseInOrderRecursion(rootNode.RightNode);
+            }
+
+        }
+
+        /// <summary>
+        /// This is level order in one line 
+        /// 1. If queue is empty and root node is not null meaning we are starting the process for the first time put root Node in queue
+        /// 2. Check if queue has some node in it. If the node in queue has a left child, enqueue the left child
+        /// 3. Check if queue has some node in it. If the node in queue has a right child, enqueue the left child
+        /// 4. Dequeue the queue and print the element
+        /// 5. If the root node has a left child, call recursion with left child as root node
+        /// 6. If root node has right child call recursion with right child as root node
+        /// </summary>
+        /// <param name="rootNode"></param>
+        public void LevelOrderTraversalUsingQueue(Node rootNode)
+        {
+            
+            if (queue.Count==0 && rootNode != null)
+            {
+                queue.Enqueue(rootNode);
+            }
+
+            if (queue.Peek() != null)
+            {
+                if(queue.Peek().LeftNode!=null) queue.Enqueue(queue.Peek().LeftNode);
+                if (queue.Peek().RightNode != null) queue.Enqueue(queue.Peek().RightNode);
+                Console.WriteLine(queue.Dequeue().Data);
+                if (queue.Count>0)
+                    LevelOrderTraversalUsingQueue(queue.Peek());
+            }
+            //if (rootNode.LeftNode != null)
+            //    LevelOrderTraversalUsingQueue(rootNode.LeftNode);
+            //if (rootNode.RightNode != null)
+            //    LevelOrderTraversalUsingQueue(rootNode.RightNode);
+        }
+        /// <summary>
+        /// This is level order level by level algorithm
+        /// 1. For the first element add it to queue and add a null after that
+        /// If queue peek is not null
+        ///     2. Check the queue and if the data is not null enqueue the left and right child
+        ///     3. Dequeue the data and print it
+        ///     4. Peek the data from queue and call the recursion using the peeked data
+        /// If queue peek is null
+        ///     5. Dequeue
+        ///     6. Write new line
+        ///     7. enqueue null 
+        ///     8. check if peeked element is not null call recursion with peeked element
+        /// 
+        /// </summary>
+        /// <param name="rootNode"></param>
+        public void LevelOrderTraversalUsingQueueLevelByLevel(Node rootNode)
+        {
+            if (queue.Count == 0 && rootNode != null)
+            {
+                queue.Enqueue(rootNode);
+                queue.Enqueue(null);
+            }
+            if (queue.Peek() != null)
+            {
+                if (queue.Peek().LeftNode != null) queue.Enqueue(queue.Peek().LeftNode);
+                if (queue.Peek().RightNode != null) queue.Enqueue(queue.Peek().RightNode);
+                Console.Write(queue.Dequeue().Data +" ");
+                LevelOrderTraversalUsingQueueLevelByLevel(queue.Peek());
+            }
+            else if(queue.Peek()==null)
+            {
+                queue.Dequeue();
+                Console.Write("\n");
+                queue.Enqueue(null);
+                if(queue.Peek()!=null)
+                    LevelOrderTraversalUsingQueueLevelByLevel(queue.Peek());
+            }
+           
+            
+
         }
     }
 }
--- BinarySearchTreeImplementation/Program.cs
+++ BinarySearchTreeImplementation/Program.cs
@@ -16,18 +16,21 @@ namespace BinarySearchTreeImplementation
             binaryTree.Add(10);
             binaryTree.Add(5);
             binaryTree.Add(8);
-            Console.WriteLine("PreOrder Traversal:");
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            binaryTree.TraversePreOrderRecursion(binaryTree.Root);
-            watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
-            Console.WriteLine("Recursion time " + elapsedMs);
-             watch = System.Diagnostics.Stopwatch.StartNew();
-            binaryTree.TraversePreOrderIteration(binaryTree.Root);
-            watch.Stop();
-            elapsedMs = watch.ElapsedMilliseconds;
-            Console.WriteLine("Iteration time " + elapsedMs);
-            Console.WriteLine();
+            //Console.WriteLine("PreOrder Traversal:");
+            //var watch = System.Diagnostics.Stopwatch.StartNew();
+            //binaryTree.TraversePreOrderRecursion(binaryTree.Root);
+            //watch.Stop();
+            //var elapsedMs = watch.ElapsedMilliseconds;
+            //Console.WriteLine("Recursion time " + elapsedMs);
+            // watch = System.Diagnostics.Stopwatch.StartNew();
+            //binaryTree.TraversePreOrderIteration(binaryTree.Root);
+            //watch.Stop();
+            //elapsedMs = watch.ElapsedMilliseconds;
+            //Console.WriteLine("Iteration time " + elapsedMs);
+            //Console.WriteLine();
+            //binaryTree.TraverseInOrderRecursion(binaryTree.Root);
+            //binaryTree.LevelOrderTraversalUsingQueue(binaryTree.Root);
+            binaryTree.LevelOrderTraversalUsingQueueLevelByLevel(binaryTree.Root);
         }
     }
 }
